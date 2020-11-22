823e86bea736eb06dfa73c3d7ca962a420684352
roma-claudio <roma.claudio.it@gmail.com>
2020-11-22 18:56:36 +0000

    Updates

---
+++ precache-manifest.f5baeddab3813669ab332e6b89bfc6f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda4f35dece7d897c2a593268fe014b5",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c63fb2bc996720dd858d",
+    "url": "/weather/static/css/main.bc3b6023.chunk.css"
+  },
+  {
+    "revision": "8b44b2db52f472bd110f",
+    "url": "/weather/static/js/2.837a6c12.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/weather/static/js/2.837a6c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63fb2bc996720dd858d",
+    "url": "/weather/static/js/main.1f28ee08.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
